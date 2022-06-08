f0a982fc640f5705f365770a6cf7c09766c051c9
joffinjoy <35325730+joffinjoy@users.noreply.github.com>
2022-06-08 12:00:01 +0530

    Temp Commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,38 +6,34 @@ const configuration = require('./config');
 const router = require('./route/routes');
 const { agenda, jobsReady } = require('./services/agendaServices');
 
-
 //express
 const app = express();
 
-const PORT = process.env.APPLICATION_PORT 
+const PORT = process.env.APPLICATION_PORT;
 
 agenda;
 jobsReady;
 
 //config
-require("./configs")
+require('./configs');
 
 //middleware definition
 app.use(bodyParser.json({ limit: '50MB' }));
 app.use(bodyParser.urlencoded({ limit: '50MB', extended: false }));
-app.use('/',router);
-
-
-
+app.use('/', router);
 
 //starting the server at port defined in config.js
-const server=app.listen( PORT,()=> {
-    console.log(`server started at: http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
+	console.log(`server started at: http://localhost:${PORT}`);
 });
 
 async function graceful() {
-    console.log("\nClosing server...");
-    await server.close();
-    console.log("Shutting down gracefully...");
-    await agenda.stop();
-    process.exit(0);
-  }
-  
-  process.on("SIGTERM", graceful);
-  process.on("SIGINT", graceful);
+	console.log('\nClosing server...');
+	await server.close();
+	console.log('Shutting down gracefully...');
+	await agenda.stop();
+	process.exit(0);
+}
+
+process.on('SIGTERM', graceful);
+process.on('SIGINT', graceful);
