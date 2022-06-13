498fdb9c7931c782a6812fe67882247d43a7418d
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-06-13 22:07:00 +0530

    src directory added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const configuration = require('./config');
 const router = require('./route/routes');
 const { agenda, jobsReady } = require('./services/agendaServices');
 
