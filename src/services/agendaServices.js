047e2825a63b2faed6b076193b2cc2829ad13044
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-06-14 10:22:27 +0530

    prettier test

--- src/services/agendaServices.js
+++ src/services/agendaServices.js
@@ -3,176 +3,159 @@
  * author : Vishnudas
  * created-date : 19-may-2022
  * Description : all common constants
-*/
+ */
 
 //Dependencies
-const Agenda = require('agenda');
-const configuration = require('../config');
-const needle = require('needle');
+const Agenda = require('agenda')
+const configuration = require('../config')
+const needle = require('needle')
 const responseMessage = require('../generics/constants/responseMessage')
 const httpResponse = require('../generics/constants/httpResponse')
 const common = require('../generics/constants/common')
 
-const {sendErrorMail} = require('../generics/utils');
-const log = require("../models/log");
-
+const { sendErrorMail } = require('../generics/utils')
+const log = require('../models/log')
+let prettier = 5
 //connect Agenda to default collection--agendaJobs
 const agenda = new Agenda({
-  db : {
-      address : configuration.mongourl,
-      collection : configuration.collection ? configuration.collection : undefined
-  }
-
-});
-
+	db: {
+		address: configuration.mongourl,
+		collection: configuration.collection ? configuration.collection : undefined,
+	},
+})
 
 //restart agenda instances when server restarts
-const jobsReady = agenda._ready.then( async () => {
-  const jobDefCollection = agenda._mdb.collection( configuration.definition );
+const jobsReady = agenda._ready.then(async () => {
+	const jobDefCollection = agenda._mdb.collection(configuration.definition)
 
-  jobDefCollection.toArray = () => {
-      const jobsCursor = jobDefCollection.find();
-      return (jobsCursor.toArray).bind(jobsCursor)();
-  };
-  await jobDefCollection
-    .toArray()
-    .then((jobsArray) =>
-      Promise.all(jobsArray.map((job) => defineJob(job, jobDefCollection, agenda)))
-    );
-  await agenda.start();
-  return jobDefCollection;
-});
-
+	jobDefCollection.toArray = () => {
+		const jobsCursor = jobDefCollection.find()
+		return jobsCursor.toArray.bind(jobsCursor)()
+	}
+	await jobDefCollection
+		.toArray()
+		.then((jobsArray) => Promise.all(jobsArray.map((job) => defineJob(job, jobDefCollection, agenda))))
+	await agenda.start()
+	return jobDefCollection
+})
 
 //Defining agenda job . job details are added to the collection.
-const defineJob = async ( job, jobs, agenda ) => {
-    let jobDef = job;
-    const{ name, request, email } = job;
-    agenda.define( name, async (job) => {
+const defineJob = async (job, jobs, agenda) => {
+	let jobDef = job
+	const { name, request, email } = job
+	agenda.define(name, async (job) => {
+		//needle is being implemented here
 
-      //needle is being implemented here
+		//adding header details
+		const options = {
+			headers: request.header ? request.header : {},
+		}
 
-        //adding header details
-        const options = {
-          headers : request.header ? request.header : {}
-        }
-        
-        await needle( request.method, request.url, options)
-        .then(function(response) {
-          
-          addExicutionLog( jobDef, common.SUCCESS, { "response" : common.SUCCESS }, job.attrs.lastRunAt );
-          return "good"
+		await needle(request.method, request.url, options)
+			.then(function (response) {
+				addExicutionLog(jobDef, common.SUCCESS, { response: common.SUCCESS }, job.attrs.lastRunAt)
+				return 'good'
+			})
+			.catch(function (err) {
+				addExicutionLog(jobDef, common.FAILED, err, job.attrs.lastRunAt)
+				//send mail notification in case of an error
+				sendErrorMail(email, jobDef, err)
+			})
+	})
 
-        })
-        .catch(function(err) {
+	await jobs
+		.countDocuments({ name })
+		.then((count) => (count < 1 ? jobs.insertOne(job) : jobs.updateOne({ name }, { $set: job })))
 
-          addExicutionLog( jobDef, common.FAILED, err, job.attrs.lastRunAt );
-          //send mail notification in case of an error
-          sendErrorMail( email, jobDef, err );
-
-        })
-        
-        
-    });
-
-    await jobs.countDocuments({ name })
-      .then((count) =>
-      
-        count < 1 ? jobs.insertOne(job) : jobs.updateOne({ name }, { $set: job })
-      );
-  
-    return ({
-        success : true
-    })
-};
+	return {
+		success: true,
+	}
+}
 
 //scheduling job for every given interval
-const scheduleEvery = async ( scheduleData, agenda ) => {
-    try {
-        const name = scheduleData.name;
-        const interval = scheduleData.interval;
-        const schedule = await agenda.every( interval, name );
-        
-        return ({
-            message : responseMessage.SHEDULE_ONCE_SUCCESS,
-            success : true,
-            status : httpResponse.OK
-        });
-    } catch ( err ){
-        return({
-            message : responseMessage.SHEDULING_FAILED + err,
-            success : false,
-            status : httpResponse.BAD_REQUEST
-        });
-    }
-}; 
+const scheduleEvery = async (scheduleData, agenda) => {
+	try {
+		const name = scheduleData.name
+		const interval = scheduleData.interval
+		const schedule = await agenda.every(interval, name)
+
+		return {
+			message: responseMessage.SHEDULE_ONCE_SUCCESS,
+			success: true,
+			status: httpResponse.OK,
+		}
+	} catch (err) {
+		return {
+			message: responseMessage.SHEDULING_FAILED + err,
+			success: false,
+			status: httpResponse.BAD_REQUEST,
+		}
+	}
+}
 
 //shedule job for immediate exicution
-const scheduleNow = async (jobName, agenda ) => {
-    try {
-        const schedule = await agenda.now( jobName );
-        
-        return ({
-            message : responseMessage.SHEDULE_ONCE_SUCCESS,
-            success : true,
-            status : httpResponse.OK
-        });
-        
-    } 
-    catch ( err ) {
-        return({
-            message : responseMessage.SHEDULING_FAILED + err,
-            success : false,
-            status : httpResponse.BAD_REQUEST
-        });
-    }
-};
+const scheduleNow = async (jobName, agenda) => {
+	try {
+		const schedule = await agenda.now(jobName)
+
+		return {
+			message: responseMessage.SHEDULE_ONCE_SUCCESS,
+			success: true,
+			status: httpResponse.OK,
+		}
+	} catch (err) {
+		return {
+			message: responseMessage.SHEDULING_FAILED + err,
+			success: false,
+			status: httpResponse.BAD_REQUEST,
+		}
+	}
+}
 
 //schedule job for once
-const scheduleOnce = async ( scheduleData, agenda ) => {
-    try {
-        const name = scheduleData.name;
-        const interval = scheduleData.interval;
-        const schedule = await agenda.schedule( interval, name );
-        
-        return ({
-            message : responseMessage.SHEDULE_ONCE_SUCCESS,
-            success : true,
-            status : httpResponse.OK
-        });
-        
-    } 
-    catch ( err ) {
-        return({
-            message : responseMessage.SHEDULING_FAILED + err,
-            success : false,
-            status : httpResponse.BAD_REQUEST
-        });
-    }
-};
+const scheduleOnce = async (scheduleData, agenda) => {
+	try {
+		const name = scheduleData.name
+		const interval = scheduleData.interval
+		const schedule = await agenda.schedule(interval, name)
+
+		return {
+			message: responseMessage.SHEDULE_ONCE_SUCCESS,
+			success: true,
+			status: httpResponse.OK,
+		}
+	} catch (err) {
+		return {
+			message: responseMessage.SHEDULING_FAILED + err,
+			success: false,
+			status: httpResponse.BAD_REQUEST,
+		}
+	}
+}
 
 //Add execution log
-const addExicutionLog = async ( job, status, resp , time ) => {
-  let report = new log ({
-    jobDetails : job,
-    runAt : time,
-    response : resp,
-    exicutionStatus : status
-  });
-  await report.save()
-   .then(doc => {
-     console.log(doc)
-   })
-   .catch(err => {
-     console.error(err)
-   })
+const addExicutionLog = async (job, status, resp, time) => {
+	let report = new log({
+		jobDetails: job,
+		runAt: time,
+		response: resp,
+		exicutionStatus: status,
+	})
+	await report
+		.save()
+		.then((doc) => {
+			console.log(doc)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
 }
 
 module.exports = {
-    defineJob,
-    agenda,
-    jobsReady,
-    scheduleEvery,
-    scheduleNow,
-    scheduleOnce
-};
+	defineJob,
+	agenda,
+	jobsReady,
+	scheduleEvery,
+	scheduleNow,
+	scheduleOnce,
+}
