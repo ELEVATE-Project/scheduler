7a0d2f9efa4c11573bf694509760efcacc050e25
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2022-06-22 09:58:41 +0530

    constants folder moved to src

--- src/constants/common.js
+++ src/constants/common.js
@@ -3,12 +3,12 @@
  * author : Vishnudas
  * created-date : 19-may-2022
  * Description : all common constants
-*/
+ */
 
 module.exports = {
-    "ONCE" : "once",
-    "NOW" : "now",
-    "EVERY" : "every", 
-    "SUCCESS" : "success",
-    "FAILED" : "failed"
-};
+	ONCE: 'once',
+	NOW: 'now',
+	EVERY: 'every',
+	SUCCESS: 'success',
+	FAILED: 'failed',
+}
---
+++ src/constants/responseMessage.js
@@ -0,0 +1,20 @@
+/**
+ * name : responseMessage.js
+ * author : Vishnudas
+ * created-date : 19-may-2022
+ * Description : all response messages
+ */
+
+module.exports = {
+	SHEDULE_ONCE_SUCCESS: 'Successfully scheduled job at given time.',
+	SHEDULING_FAILED: 'Failed to schedule the job',
+	JOB_DEFINITION_NOT_FOUND: 'Job definition not found',
+	JOB_DEFINITION_ALREADY_EXISTS: 'Job definition already exists',
+	JOB_LIST_FETCH_SUCCESS: 'Sucessfully fetched job list',
+	JOB_LIST_FETCH_FAILED: 'Job list fetching failed',
+	JOB_INSTANCE_CANCELLED: '- job instance cancelled',
+	INSTANCE_NOT_PRESENT: 'No job instance exist in specified job name',
+	JOB_DELETED: 'Successfully deleted job data',
+	JOB_NOT_FOUND: 'Job not found',
+	JOB_UPDATED: 'Successfully updated job',
+}
