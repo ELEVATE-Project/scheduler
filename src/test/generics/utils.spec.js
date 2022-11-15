cbbc195d5013dabbace3cc5bf1c50d0279e7c35a
Nevil <nevil@tunerlabs.com>
2022-11-15 14:45:27 +0530

    added unit test

---
+++ src/test/generics/utils.spec.js
@@ -0,0 +1,33 @@
+/* async function loadMongo() {
+	let db = await mongoose.connect(global.__MONGO_URI__ + global.mongoDBName, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	global.db = db
+} */
+
+describe('Utils', () => {
+	let utils
+
+	beforeAll(async () => {
+		//await loadMongo()
+		utils = require('@generics/utils')
+		return
+	})
+
+	test('Should return md5 hash', async () => {
+		const actual = await utils.md5Hash('6329891e13fa52673aeb8c4f')
+		expect(actual).toEqual('c3bf9eef82ce2ef0c4bc11a2124ec2a7')
+	})
+	/* 	afterAll(async () => {
+		try {
+			mongoose.connection.close()
+		} catch (error) {
+			console.log(`
+            You did something wrong
+            ${error}
+          `)
+			throw error
+		}
+	}) */
+})
