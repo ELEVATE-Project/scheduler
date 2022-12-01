ed0b56b8940aa5f443e4197c34f0c36aca7afc36
rakesh kumar <rakeshkumar@rakeshs-MacBook-Air.local>
2022-12-01 13:05:25 +0530

    kafka and mongo cluster changes

--- src/generics/kafka-communication.js
+++ src/generics/kafka-communication.js
@@ -3,28 +3,28 @@
  * author : Vishnudas
  * Date : 12-may-2022
  * Description : Kafka producer methods
-*/
+ */
 
-const pushEmailToKafka = async message => {
-    try {
-        const payload = [{ topic: process.env.NOTIFICATION_KAFKA_TOPIC, messages: JSON.stringify(message) }];
-        return await pushPayloadToKafka(payload)
-    } catch (error) {
-        throw error;
-    }
-};
+const pushEmailToKafka = async (message) => {
+	try {
+		const payload = { topic: process.env.NOTIFICATION_KAFKA_TOPIC, messages: [{ value: JSON.stringify(message) }] }
+		return await pushPayloadToKafka(payload)
+	} catch (error) {
+		throw error
+	}
+}
 
 const pushPayloadToKafka = (payload) => {
-    return new Promise((resolve, reject) => {
-        kafkaProducer.send(payload, (error, data) => {
-            if (error) {
-                reject(error);
-            }
-            resolve(data);
-        });
-    });
-};
+	return new Promise(async function (resolve, reject) {
+		let response = await kafkaProducer.send(payload)
+		if (response) {
+			resolve(response)
+		} else {
+			reject(false)
+		}
+	})
+}
 
 module.exports = {
-    pushEmailToKafka
-};
+	pushEmailToKafka,
+}
