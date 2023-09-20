b9f34b180ee7f87a29844fa8eedd6b9e0a9d4779
joffinjoy <joffinjoy@gmail.com>
2023-09-20 12:08:07 +0530

    Added MongoDB Version Information

--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -5,6 +5,12 @@
  * Description : Mongodb connections configurations
  */
 
+/* 
+Uses MongoDB v4.1.4, which has an OSI Compliant License (GNU Affero General Public License, version 3)
+MongoDB v4.1.4 repository: https://github.com/mongodb/mongo/tree/r4.1.4
+MongoDB v4.1.4 License: https://github.com/mongodb/mongo/blob/r4.1.4/LICENSE-Community.txt
+*/
+
 //Dependencies
 const mongoose = require('mongoose')
 const { elevateLog } = require('elevate-logger')
