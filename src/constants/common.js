681f6bfb96c66d2517bbdc54c72d4f724edfb20e
joffinjoy <joffinjoy@gmail.com>
2024-04-23 10:51:39 +0530

    Reverting To 2.5.0 Spec

---
+++ src/constants/common.js
@@ -0,0 +1,34 @@
+const { elevateLog, correlationId } = require('elevate-logger')
+const logger = elevateLog.init()
+
+/**
+ * name : common.js
+ * author : Vishnudas
+ * created-date : 19-may-2022
+ * Description : all common constants
+ */
+
+const successResponse = ({ message, success = true, status = 200, result = {}, meta = {} }) => {
+	let response = {
+		message,
+		success,
+		status,
+		result,
+		meta: { ...meta, correlation: correlationId.getId() },
+	}
+	logger.info('Request Response', { response: response })
+
+	return response
+}
+
+const failureResponse = ({ message, success = false, status = 500 }) => {
+	return {
+		message,
+		success,
+		status,
+	}
+}
+module.exports = {
+	successResponse,
+	failureResponse,
+}
